082267186d6a9af00772baef316bd19c164ac3b9
preethamk2000 <38404919+preethamk2000@users.noreply.github.com>
2018-06-08 22:46:44 +0530

    Add files via upload

---
+++ task2.bash
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+#top 20 ip count
+
+cat /root/Desktop/web_logs.txt | awk  '{print $2}' | uniq -c | sort -nr | head -20
+
+#no of occurrences of a word in a file
+
+echo "Enter filename:"
+read fname
+echo "Enter the word to search for:"
+read word
+
+tr " " '\n' < $fname | grep $word | wc -l 
+
+#search-and-replace and we should give the file name , word and word to replace with in the terminal
+
+fname=$1
+word=$2
+rword=$3
+
+sed -i 's/'$word'/'$rword'/g' $fname
+
+#11 most frequent commands
+
+awk '{ print $1 }' ~/.bash_history | sort | uniq -c | sort -nr | head -11 
+
+#sort the files and directories inside it by size and display it
+
+ls -RSl   #this lists the files within the top level directory and arranges them correctly
+
+du -sh * | sort -rn  #this lists the top level directories in correct order by size
+
+#find empty files not accessed for 14 days and delete them and we have to give the location as input in the terminal
+
+find $1 -empty -type f -ctime +14 -exec rm -rf {} \;
+
+
